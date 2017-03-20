4d117f3e16564d0bb483375024310812a51ebcd7
limei <903075520@qq.com>
2017-03-20 20:22:10 +0800

    111

---
+++ js/controller/login/appCtrl.js
@@ -0,0 +1,8 @@
+/**
+ * Created by yhg97p on 2017/3/17.
+ */
+    define(['app'],function(app){
+        app.register.controller('appCtrl',function($scope){
+            $scope.names=[1,2,3,4]
+        })
+    })
