56f5d899306e70087101746f8d65aa7473cb392b
limei <903075520@qq.com>
2017-03-20 20:22:29 +0800

    Revert "111"
    This reverts commit 4d117f3e16564d0bb483375024310812a51ebcd7.

--- js/controller/login/appCtrl.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by yhg97p on 2017/3/17.
- */
-    define(['app'],function(app){
-        app.register.controller('appCtrl',function($scope){
-            $scope.names=[1,2,3,4]
-        })
-    })
